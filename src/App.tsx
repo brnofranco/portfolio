7522329239ab1bea2c478ad8593384357426cf3b
Bruno Franco <brunofrancodemoraes@gmail.com>
2023-01-28 23:07:09 -0300

    migrando scroll to top para tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import { Layout } from "./styles/layout";
+import "./styles/global.css";
+
 import { Header } from "./components/Header";
 import { About } from "./components/About";
 import { Knowledge } from "./components/Knowledge";
 import { Certifications } from "./components/Certifications";
 import { Projects } from "./components/Projects";
-import { ScrollToTop } from "./components/ScrollToTop";
+import { ScrollToTopButton } from "./components/ScrollToTop";
 
 export function App() {
 	return (
 		<>
 			<Header />
-			<Layout>
+			<main className="flex flex-col ml-percent20 mobile:ml-0">
 				<About />
 				<Knowledge />
 				<Certifications />
 				<Projects />
-				<ScrollToTop />
-			</Layout>
+				<ScrollToTopButton />
+			</main>
 		</>
 	);
 }
--- src/components/ScrollToTop/index.tsx
+++ src/components/ScrollToTop/index.tsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react";
-import { ScrollToTopButton } from "./styles";
 import { BsFillArrowUpCircleFill } from "react-icons/bs";
 
-export function ScrollToTop() {
+export function ScrollToTopButton() {
 	const [isVisible, setIsVisible] = useState(false);
 
-	const toggleVisibility = () =>
-		window.scrollY > 250 ? setIsVisible(true) : setIsVisible(false);
+	const toggleVisibility = () => setIsVisible(window.scrollY > 250);
 
 	const scrollToTop = () => {
 		window.scrollTo({
@@ -24,11 +22,18 @@ export function ScrollToTop() {
 	}, []);
 
 	return (
-		<ScrollToTopButton
-			opacity={isVisible ? "1" : "0"}
-			cursor={isVisible ? "pointer" : "default"}
-		>
-			<BsFillArrowUpCircleFill className="icon" onClick={scrollToTop} />
-		</ScrollToTopButton>
+		<>
+			{isVisible && (
+				<div
+					id="ScrollToTopButton"
+					className="cursor-pointer transition-all ease-in-out duration-1000 fixed bottom-12 right-12 w-14 h-14 mobile:bottom-3 mobile:right-3 mobile:w-10 mobile:h-10"
+				>
+					<BsFillArrowUpCircleFill
+						className="h-full w-full text-customGreen rounded-percent50 shadow-scrollToTopButton mobile:shadow-mobileScrollToTopButton"
+						onClick={scrollToTop}
+					/>
+				</div>
+			)}
+		</>
 	);
 }
