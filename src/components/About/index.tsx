7368436379270960d29728526fc034d55f1aa6c9
Bruno Franco <brunofrancodemoraes@gmail.com>
2023-06-20 14:26:35 -0300

    Revert "Revert "migrando About para tailwind""
    This reverts commit eab97b456c5b3b4bd28a69a92357917996079943.

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,29 +1,29 @@
-import { SectionContainer } from "./styles";
 import aboutImage from "../../assets/about.svg";
 
 export function About() {
 	return (
-		<SectionContainer>
-			<div className="about-content">
-				<h1>
+		<section className="w-4/5 my-0 mx-auto h-screen p-8 flex flex-row items-center justify-center gap-8 mobile:flex-col">
+			<div className="flex flex-1 flex-col gap-8 mobile:flex-none">
+				<h1 className="text-title font-semibold text-3xl">
 					Muito prazer, sou <br />
-					<strong>Bruno Franco de Moraes</strong>
+					<strong className="text-customGreen text-5xl">
+						Bruno Franco de Moraes
+					</strong>
 				</h1>
 
-				<p>
+				<p className="text-text text-lg">
 					Busco aprendizado constante na minha carreira como
 					desenvolvedor para, futuramente, me tornar um desenvolvedor
 					Fullstack.
 				</p>
 			</div>
-			<div className="about-image">
+			<div className="flex justify-center items-center flex-1 mobile:flex-none">
 				<img
 					src={aboutImage}
-					width="400"
-					height="400"
+					className="w-full h-fixed400"
 					alt="rapaz programador"
 				/>
 			</div>
-		</SectionContainer>
+		</section>
 	);
 }
